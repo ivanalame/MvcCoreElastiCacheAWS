ed466c07cc31ca922c346394aba8ec67e5fe3494
ivanalame <alumno.399451@ies-azarquiel.es>
2024-05-21 13:25:39 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            //aqui ira nuestra cadena de conexion
+            return ConnectionMultiplexer.Connect("");
+        });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
